3d96c88e4cf507230012056f392df1ac91865e67
Alvaro Renato Paredes Flores <41393264+alvarorpf@users.noreply.github.com>
2020-11-18 14:56:17 -0400

    Segundo Apartado

---
+++ Operadores/01-OperadoresAritmeticos.py
@@ -0,0 +1,27 @@
+# Uso de Operadores Aritmeticos en Python
+
+# Suma
+a = 3
+b = 2
+suma = a + b
+print("Suma:", suma)
+
+# Resta
+resta = a - b
+print("Resta:", resta)
+
+# Multiplicacion
+multi = a * b
+print("Multiplicacion:", multi)
+
+# Division
+division = a / b
+print("Division:", division)
+
+# Modulo
+modulo = a % b
+print("Modulo:", modulo)
+
+# Potencia
+potencia = a ** b
+print("Potencia:", potencia)
---
+++ Operadores/02-OperadoresAsignacion.py
@@ -0,0 +1,22 @@
+# Uso de Operadores de Asignacion
+
+# Operador "=" este nos sirve para asignar valores a nuestras variables
+x = 3
+y = 5
+print("X:", x,"Y:", y)
+
+# Operador de Asignacion de Suma "+=" es lo mismo que x = x + 2
+x += 2
+print("Suma: ", x)
+
+# Operador de Asignacion de Resta "-=" es lo mismo que x = x - 2
+x -= 2
+print("Resta: ", x)
+
+# Operador de Asignacion de Multiplicacion "*=" es lo mismo que x = x * 2
+y *= 2
+print("Multiplicacion:", y)
+
+# Operador de Asignacion de Division "/=" es lo mismo que x = x / 2
+y /= 2
+print("Division: ", y)
